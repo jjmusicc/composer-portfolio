c11bc4c5ebda02f8d60a10efd0ee4ed930895a95
jjmusicc <dlwowns003@gmail.com>
2025-08-11 16:58:51 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        librebaskerville: ['"Libre Baskerville"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
